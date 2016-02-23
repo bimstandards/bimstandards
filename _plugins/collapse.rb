6655ed8cca675db57b9e8e1c7a36e94127a6a75b
alexandregrignon <alexandre.grignon@gmail.com>
2016-02-23 21:43:13 +0100

    add font awesome

--- _plugins/collapse.rb
+++ _plugins/collapse.rb
@@ -15,7 +15,7 @@ module Jekyll
         site = context.registers[:site]
         converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
         output = converter.convert(super(context))
-        "<div class=\"collapse-group\"><div class=\"btn-group\" data-toggle=\"button\"><a class=\"btn btn-info-outline btn-sm dropdown-toggle\" data-toggle=\"collapse\" href=\"#collapse#{@random}\" aria-expanded=\"false\" aria-controls=\"collapse#{@random}\">#{@type}</a></div></div><div class=\"collapse\" id=\"collapse#{@random}\"><div class=\"card card-block\">#{output}</div></div>"
+        "<div class=\"collapse-group\"><div class=\"btn-group\" data-toggle=\"button\"><a class=\"btn btn-secondary btn-sm dropdown-toggle\" data-toggle=\"collapse\" href=\"#collapse#{@random}\" aria-expanded=\"false\" aria-controls=\"collapse#{@random}\"><i class=\"fa fa-info-circle\"></i> #{@type}</a></div></div><div class=\"collapse\" id=\"collapse#{@random}\"><div class=\"card card-block\">#{output}</div></div>"
       end
     end
   end
